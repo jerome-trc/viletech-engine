53bc6ce005eb4e44ac43582253695859f49f4323
jerome-trc <46534306+jerome-trc@users.noreply.github.com>
2024-02-26 18:20:30 -0500

    Cargo workspace boilerplate and client crate

---
+++ crates/client/build.rs
@@ -0,0 +1,40 @@
+use std::path::Path;
+
+fn main() {
+	let mut config = cbindgen::Config::default();
+	config.language = cbindgen::Language::Cxx;
+	config.include_version = true;
+	config.namespace = Some("vtec".to_string());
+	config.no_includes = true;
+	config.pragma_once = true;
+	config.macro_expansion.bitflags = true;
+	config.structure.associated_constants_in_body = true;
+
+	gen_header(&mut config, "", "viletech.hpp", &[]);
+
+	cbindgen::generate_with_config(std::env::var("CARGO_MANIFEST_DIR").unwrap(), config.clone())
+		.expect("binding generation failed")
+		.write_to_file("build/viletech.hpp");
+}
+
+fn gen_header(
+	config: &mut cbindgen::Config,
+	header: &'static str,
+	rel_path: &'static str,
+	symbols: &'static [&'static str],
+) {
+	if !header.is_empty() {
+		config.header = Some(format!("//! @file\n//! @brief {header}"));
+	}
+
+	for sym in symbols {
+		config.export.include.push(sym.to_string());
+	}
+
+	cbindgen::generate_with_config(std::env::var("CARGO_MANIFEST_DIR").unwrap(), config.clone())
+		.expect("binding generation failed")
+		.write_to_file(Path::new("../../build").join(rel_path));
+
+	config.export.include.clear();
+	config.header = None;
+}
---
+++ crates/client/src/lib.rs
@@ -0,0 +1,27 @@
+//! # VileTech: CLAP
+//!
+//! Command line argument parsing.
+
+use clap::Parser;
+
+#[derive(clap::Parser, Debug)]
+#[command(name = "VileTech Client")]
+#[command(version)]
+#[command(about = "Play id Tech 1 games on the VileTech Engine")]
+#[command(long_about = "
+VileTech Client - Copyright (C) 2024 - jerome-trc
+
+This program comes with ABSOLUTELY NO WARRANTY.
+
+This is free software, and you are welcome to redistribute it under certain
+conditions. See the license document that comes with your installation.")]
+struct LaunchArgs {
+	/// Use original dsda-doom code for demo compatibility.
+	#[arg(short, long)]
+	legacy: bool,
+}
+
+#[no_mangle]
+pub extern "C" fn parse_args() {
+	let args = LaunchArgs::parse();
+}
