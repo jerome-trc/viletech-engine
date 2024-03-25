2e9fc40f8964849b1aaca49683f57d40faba4c50
jerome-trc <46534306+jerome-trc@users.noreply.github.com>
2024-03-25 15:35:07 -0400

    Formatting/organizational changes in flecs.nim

--- client/src/flecs.nim
+++ client/src/flecs.nim
@@ -11,10 +11,10 @@ type
     EntityDesc* {.header: flecs, importc: "ecs_entity_desc_t".} = object
         name*: cstring
 
-proc `==` *(a, b: Entity): bool {.borrow.}
-
 const
     NullEntity* = 0.Entity
+
+proc `==` *(a, b: Entity): bool {.borrow.}
 
 proc initWorld*(): World {.importc: "ecs_init".}
 
@@ -23,21 +23,21 @@ proc reset*(this: World): cint {.importc: "ecs_fini".}
 proc newId*(this: World) {.importc: "ecs_new_id".}
 
 proc initEntity*(
-        this: World,
-        desc {.codegenDecl: "const $1 $2".}: ptr EntityDesc
-    ): Entity {.importc: "ecs_entity_init".}
+    this: World,
+    desc {.codegenDecl: "const $1 $2".}: ptr EntityDesc
+): Entity {.importc: "ecs_entity_init".}
 
 proc lookup*(
-        this {.codegenDecl: "const $1 $2".}: World,
-        path {.codegenDecl: "const char* $2".}: cstring
-    ): Entity {.importc: "ecs_lookup".}
+    this {.codegenDecl: "const $1 $2".}: World,
+    path {.codegenDecl: "const char* $2".}: cstring
+): Entity {.importc: "ecs_lookup".}
 
 proc getName*(this: World, entity: Entity) {.importc: "ecs_get_name".}
 
 template ecsComponent*(
-        world {.codegenDecl: "$1 world".}: untyped,
-        componentT: untyped
-    ) =
+    world {.codegenDecl: "$1 world".}: untyped,
+    componentT: untyped
+) =
     block:
         # `ECS_COMPONENT` can only be used in a C function, so generate one.
         proc `ecs componentT`(world {.codegenDecl: "$1 world".}: World) {.inject.} =
